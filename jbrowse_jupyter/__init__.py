436dd0faf303795c8d44e89531efed2c6707c631
teresam856 <tere486martinez@gmail.com>
2021-11-22 17:11:39 -0800

    update demos

--- jbrowse_jupyter/__init__.py
+++ jbrowse_jupyter/__init__.py
@@ -1,2 +1,2 @@
 from jbrowse_jupyter.jbrowse_config import JBrowseConfig, create_jbrowse2
-from jbrowse_jupyter.util import is_URL, create_component
+from jbrowse_jupyter.util import is_URL, launch
--- jbrowse_jupyter/util.py
+++ jbrowse_jupyter/util.py
@@ -3,6 +3,8 @@ import json
 import re
 import os
 from pathlib import Path
+import dash_html_components as html
+from jupyter_dash import JupyterDash
 
 
 def is_URL(filePath):
@@ -21,11 +23,22 @@ def defaults(name):
         conf = data
         return conf
 
-def create_component(conf):
-    return dash_jbrowse.DashJbrowse(
-        id="jbrowse-component",
-        assembly=conf["assembly"],
-        tracks=conf["tracks"],
-        defaultSession=conf["defaultSession"],
-        location=conf["location"],
-    )
+# def create_component(conf):
+#     return dash_jbrowse.DashJbrowse(
+#         id="jbrowse-component",
+#         assembly=conf["assembly"],
+#         tracks=conf["tracks"],
+#         defaultSession=conf["defaultSession"],
+#         location=conf["location"],
+#     )
+
+def launch(conf):
+    app = JupyterDash(__name__)
+    app.layout = html.Div([dash_jbrowse.DashJbrowse(
+            id="jbrowse-component",
+            assembly=conf["assembly"],
+            tracks=conf["tracks"],
+            defaultSession=conf["defaultSession"],
+            location=conf["location"],
+        )])
+    app.run_server(port=3000, mode="inline")        
