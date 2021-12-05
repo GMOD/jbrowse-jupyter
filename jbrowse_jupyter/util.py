0501ea121bbfd8a70920f16c43ddd781b1b129ea
teresam856 <tere486martinez@gmail.com>
2021-12-05 15:38:40 -0800

    add docstrings to the util file

--- jbrowse_jupyter/util.py
+++ jbrowse_jupyter/util.py
@@ -10,10 +10,25 @@ from jupyter_dash import JupyterDash
 
 
 def is_URL(filePath):
+    """
+    Checks wether or not the file path
+    is a valid url.
+    :param str filePath: file path
+    :return: returns true if path matches pattern starting with
+    http:// or https://
+    :rtype: boolean
+    """
     regex = re.compile(r'^https?:\/\/', re.IGNORECASE)
     return re.match(regex, filePath) is not None
 
 def guess_file_name(data):
+    """
+    Guess the file name given a path.
+
+    :param str data: file path
+    :return: the predicted file name
+    :rtype: str
+    """
     url = urlparse(data)
     return os.path.basename(url.path)
 
@@ -36,6 +51,7 @@ def get_name(assembly_file):
     return assembly_file[name_start:name_end]
 
 def defaults(name):
+    """Returns the configuration oject given a genome name."""
     os.getcwd()
     fileName = Path(f'jbrowse_jupyter/data/{name}.json').resolve()
     with open(fileName, "r") as file:
@@ -44,6 +60,10 @@ def defaults(name):
         return conf
 
 def create_component(conf, **kwargs):
+    """ 
+    Creates a Dash JBrowse LinearGenomeView component given a
+        configuration object and optionally an id.
+    """
     comp_id = "jbrowse-component"
     if "component_id" in kwargs:
         comp_id = kwargs["component_id"]
@@ -57,16 +77,25 @@ def create_component(conf, **kwargs):
     )
 
 def launch(conf, **kwargs):
+    """
+    Launches a LinearGenomeView Dash component.
+
+    :param obj conf: JBrowseConfiguration object to pass to the Dash JBrowse component
+    :param str id: (optional) id to use for the Dash JBrowse component
+        defaults to `jbrowse-component`
+    :param int port: (optional) port to utilize when running the JupyterDash app server
+    :param int height: (optional) the height to utilize for the JupyterDash app
+    """
     # TODO: add setting custom text search adapters
     comp_id = "jbrowse-component"
     comp_port = 3000
     comp_height = 300
-    if "component_id" in kwargs:
-        comp_id = kwargs["component_id"]
+    if "id" in kwargs:
+        comp_id = kwargs["id"]
     if "port" in kwargs:
         comp_port = kwargs["port"]
-    if "component_height" in kwargs:
-        comp_height = kwargs["component_height"]
+    if "height" in kwargs:
+        comp_height = kwargs["height"]
 
     app = JupyterDash(__name__)
     app.layout = html.Div([LinearGenomeView(
