02d3dc570feca12ddeb7fa8e5ddf27ed8e159d3b
teresam856 <tere486martinez@gmail.com>
2022-02-02 13:47:40 -0800

    reorganize tutorial and update docs

--- cgv_examples.py
+++ cgv_examples.py
@@ -5,6 +5,7 @@ app = Dash(__name__)
 base = "https://jbrowse.org/genomes/hg19/fasta/"
 base2 = "https://s3.amazonaws.com/jbrowse.org/genomes/hg19/"
 file_name = 'hs37d5.HG002-SequelII-CCS.bnd-only.sv.vcf.gz'
+
 my_assembly = {
     "name": "hg19",
     "aliases": ["GRCh37"],
@@ -79,27 +80,37 @@ my_default_session = {
         }],
     },
 }
-# create config and pass additional params
+
+# ===== Aproach 1 ======
+
+# Creating my own conf obj to use when creating a JBrowseConfig
 config = {
     "assembly": my_assembly,
     "defaultSession": my_default_session,
     "tracks": my_tracks
 }
 cgv_with_conf = create(view_type="CGV", conf=config)
+component = create_component(cgv_with_conf.get_config(), dash_comp="CGV")
+# ===== Aproach 2 ======
+
+# Using one of the provided default genomes
+cvg_default = create("CGV", genome="hg19").get_config()
+component2 = create_component(cvg_default, id="test2", dash_comp="CGV")
 
 
+# ===== Aproach 3 ======
+
+# Using the JBrowseConfig API to build a CGV conf from scratch
 cgv_with_api = create(view_type="CGV")
 cgv_with_api.set_assembly(f'{base}hg19.fa.gz', aliases=["GRCh37"])
 cgv_with_api.add_track(f'{base2}pacbio/{file_name}', track_id="variantTest")
 cgv_with_api.set_default_session(["variantTest"], False)
-cgv_with_api.set_theme("#311b92", "#0097a7", "#f57c00", "#d50000")
-# create a dash component
-component = create_component(cgv_with_conf.get_config(), dash_comp="CGV")
-cvg_default = create("CGV", genome="hg19").get_config()
-component2 = create_component(cvg_default, id="test2", dash_comp="CGV")
+# cgv_with_api.set_theme("#311b92", "#0097a7", "#f57c00", "#d50000")
 cgv3 = cgv_with_api.get_config()
 component3 = create_component(cgv3, id="test3", dash_comp="CGV")
-# launch the component
+
+
+
 # 3 different ways to configure, 3 identical components
 app.layout = html.Div(
     [component, component2, component3],
