528145ef1b96c8e72f93695e641eaf3f2d4c8218
teresam856 <tere486martinez@gmail.com>
2022-06-23 13:50:22 -0700

    update docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2021, Teresa De Jesus Martinez, JBrowse Team'
 author = 'Teresa De Jesus Martinez, JBrowse Team'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.6'
+release = '1.2.8'
 
 
 # -- General configuration ---------------------------------------------------
