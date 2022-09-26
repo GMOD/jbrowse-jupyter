618c24f7518c6c2fac5fbf3c9c2ba61313fe1290
teresam856 <tere486martinez@gmail.com>
2022-09-25 21:17:23 -0700

    add defaults to serve script

--- serve.py
+++ serve.py
@@ -4,19 +4,21 @@ from jbrowse_jupyter import serve
 
 if __name__ == "__main__":
     # Enter a PORT number
-    PORT = input("ENTER A PORT: ")
+    PORT = input("Enter a PORT: [default:8080] > ")
     if not PORT:
         PORT = 8080
     else:
         PORT = int(PORT)
+    print(PORT)
     # Enter a host
-    HOST = input("ENTER A HOST: ")
+    HOST = input("ENTER A HOST:  [default:localhost] > ")
     if not HOST:
         HOST = "localhost"
+    print(HOST)
     # Enter the path to your local data
-    DIRECTORY = input("ENTER PATH TO YOUR LOCAL DATA: ")
+    DIRECTORY = input("ENTER PATH TO YOUR LOCAL DATA: [default:(pwd)] > ")
     if not DIRECTORY:
         DIRECTORY = os.getcwd()
+    print(DIRECTORY)
+    print("\n")
     serve(DIRECTORY, port=PORT, host=HOST)
-    print("\n You are now running a development server. \n"
-          "This is not inteded for production")
