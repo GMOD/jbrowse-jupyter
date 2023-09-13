ac2300d26cd84bb920f348de0246028c0e82b905
vdejager <info@bioview.nl>
2023-09-13 13:50:14 +0200

    changed order in get_env

--- jbrowse_jupyter/jbrowse_config.py
+++ jbrowse_jupyter/jbrowse_config.py
@@ -157,11 +157,11 @@ class JBrowseConfig:
         return self.jupyter
 
     def get_env(self):
-        print("notebook port: ", self.nb_port)
-        print("notebook host: ", self.nb_host)
         print("notebook scheme: ", self.nb_scheme)
+        print("notebook host: ", self.nb_host)
+        print("notebook port: ", self.nb_port)
         
-        return nb_scheme, self.nb_host, self.nb_port 
+        return self.nb_scheme, self.nb_host, self.nb_port 
 
     def set_env(self, notebook_scheme="http", notebook_host="localhost", notebook_port=8888):
         """
